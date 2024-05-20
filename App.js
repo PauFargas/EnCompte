aafb4698daeb81b4707c07c8184f20c0c0926d15
Pau Fargas <pfargas.iesrcc@gmail.com>
2024-05-20 16:14:22 +0200

    Navegació a pantalla de joc i safeArea

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-
+import { useState } from 'react';
 
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+
+  function pickedNumberHandler(pickedNumber){
+    setUserNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return  (
   <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
     <ImageBackground 
@@ -12,7 +24,9 @@ export default function App() {
     resizeMode='cover' 
     style={styles.rootScreen}
     imageStyle={styles.backgroundImage}>
-      <StartGameScreen/>
+      <SafeAreaView style={styles.rootScreen}>
+      {screen}
+      </SafeAreaView>
     </ImageBackground>
   </LinearGradient>
 );
